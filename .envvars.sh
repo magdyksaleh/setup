a7b31951b3d9bf8a0cc32a9ace41d183a9be3995
Magdy Saleh <magdy@predibase.com>
2023-03-15 21:08:08 -0400

    add envvars

---
+++ .envvars.sh
@@ -0,0 +1,21 @@
+export PATH=$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin:/opt/homebrew/bin:/opt/homebrew/opt/openjdk/bin:$PATH
+export PATH=/usr/local/opt/libpq/bin:$PATH
+export PATH=/Users/magdy/dev-scripts/bin:$PATH
+export PATH=/Users/magdy/.opam/default/bin:$HOME/.pcli/bin/:$PATH
+
+export HDF5_DIR="/opt/homebrew/opt/hdf5"
+export EDITOR=vim
+export PREDIBASE_DEV_SCRIPTS_PATH=/Users/magdy/dev-scripts
+
+export USE_CUSTOM_USAA_W2V=1
+export ZSH_AUTOSUGGEST_USE_ASYNC=1
+export WORKON_HOME=$HOME/.virtualenvs
+export PROJECT_HOME=$HOME/Devel
+export CPPFLAGS="-I/opt/homebrew/opt/openjdk/include"
+export JAVA_HOME=$(/usr/libexec/java_home)
+export NVM_DIR="$HOME/.nvm"
+export KUBE_PS1_SYMBOL_ENABLE=false
+export OPAM_SWITCH_PREFIX='/Users/magdy/.opam/default'
+export CAML_LD_LIBRARY_PATH='/Users/magdy/.opam/default/lib/stublibs:/Users/magdy/.opam/default/lib/ocaml/stublibs:/Users/magdy/.opam/default/lib/ocaml'
+export OCAML_TOPLEVEL_PATH='/Users/magdy/.opam/default/lib/toplevel'
+export PATH="$PATH:/Users/magdy/go/bin"
